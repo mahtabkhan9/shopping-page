f474ce33f5bf878037b2a2f1dc80e7cdef0e842d
mahtabkhan9 <152673406+mahtabkhan9@users.noreply.github.com>
2024-10-18 17:15:43 +0530

    first change

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Spinner.css'
+
+const Spinner = () => {
+  return (
+    <div className='h-full w-full flex justify-center items-center'>
+      <div className="spinner"></div>
+    </div>
+  )
+}
+
+export default Spinner
